a94c2caa2d501298de816dfe4b46bb8fb76eb8e0
kschouft42 <kschouft42@gmail.com>
2024-03-07 00:59:27 +0100

    fix

--- src/map.c
+++ src/map.c
@@ -13,86 +13,6 @@ void    print_cpy_map(char **cpy_map)
     }
 }
 
-char    **make_cpy_map(t_data *data)
-{
-    char **cpy_map;
-    int i;
-
-    i = 0;
-    cpy_map = malloc(sizeof(char *) * (data->map_height + 1));
-    while (i < data->map_height)
-    {
-        cpy_map[i] = malloc(sizeof(char) * (data->map_lenght + 1));
-        i++;
-    }
-    return (cpy_map);
-}
-
-void    fill_cpy_map(t_data *data, char **cpy_map)
-{
-    int x;
-    int y;
-
-    y = 0;
-    while (y < data->map_height)
-    {
-        x = 0;
-        while (x < data->map_lenght)
-        {
-            if (data->map[y][x] == ' ')
-                cpy_map[y][x] = '1';
-            else if (data->map[y][x] != '1' && data->map[y][x] != '0')
-                cpy_map[y][x] = '\0';
-            else
-                cpy_map[y][x] = data->map[y][x];
-            x++;
-        }
-        y++;
-    }
-    cpy_map[y] = NULL;
-}
-
-int	try_move_path(char **map, int x, int y, int i)
-{
-	if (i == 1)
-		if (map[y][x + 1] && map[y][x + 1] == '0')
-			return (i);
-	if (i == 2)
-		if (x > 0 && map[y][x - 1] && map[y][x - 1] == '0')
-			return (i);
-	if (i == 3)
-		if (map[y + 1][x] && map[y + 1][x] == '0')
-			return (i);
-	if (i == 4)
-		if (y > 0 && map[y - 1][x] && map[y - 1][x] == '0')
-			return (i);
-	return (0);
-}
-
-void	make_move_path(char **map, int x, int y, int i)
-{
-	if (i == 1)
-		(map[y][x + 1] = 'A');
-	if (i == 2)
-		(map[y][x - 1] = 'A');
-	if (i == 3)
-		(map[y + 1][x] = 'A');
-	if (i == 4)
-		(map[y - 1][x] = 'A');
-}
-
-void    check_is_closed(char **map, int x, int y)
-{
-    if (y == 0 || !map[y - 1][x])
-        printf("error\n");
-    else if (!map[y + 1][x])
-        printf("error\n");
-    else if (x == 0 || !map[y][x - 1])
-        printf("error\n");
-    else if (!map[y][x + 1])
-        printf("error\n");
-}
-
 int     unclosed_map(t_data *data, char **map, int x, int y)
 {
     if (y == 0 || x == 0)
@@ -110,72 +30,24 @@ int     unclosed_map(t_data *data, char **map, int x, int y)
     return (0);
 }
 
-int check_valid_path(t_data *data, char **map, int x, int y)
-{
-	int	i;
-
-	i = 1;
-	while (i < 5)
-	{
-        if (unclosed_map(data, map, x, y))
-        {
-            printf("error\n");
-            return (0);
-        }
-		else if (try_move_path(map, x, y, i) > 0)
-		{
-			make_move_path(map, x, y, i);
-			if (i == 1)
-				x = x + 1;
-			if (i == 2)
-				x = x - 1;
-			if (i == 3)
-				y = y + 1;
-			if (i == 4)
-				y = y - 1;
-			check_valid_path(data, map, x, y);
-		}
-		i++;
-	}
-	return (1);
-}
-
-int init_check_closed(t_data *data, char **map)
+int check_map(t_data *data)
 {
     int x;
     int y;
 
     y = 0;
-    while (map[y])
+    while (data->map[y])
     {
         x = 0;
-        while (map[y][x])
+        while (data->map[y][x])
         {
-            if (map[y][x] == '0')
-            {
-                map[y][x] = 'A';
-                if (!check_valid_path(data, map, x, y))
+            if (data->map[y][x] == '0')
+                if (unclosed_map(data, data->map, x, y))
                     return (0);
-            }
             x++;
         }
         y++;
     }
-    return (1);
-}
-
-int check_map(t_data *data)
-{
-    printf("map height : %d\n", data->map_height);
-    printf("map len : %d\n", data->map_lenght);
-    char **cpy_map;
-
-    cpy_map = make_cpy_map(data);
-    fill_cpy_map(data, cpy_map);
-    // print_cpy_map(cpy_map);
-    if (!init_check_closed(data, cpy_map))
-        return (0);
-    // print_cpy_map(cpy_map);
     return (1);
 }
 
@@ -188,5 +60,6 @@ int check_map_init(t_data *data)
     }
     if (check_map(data))
         return (1);
+    printf("map error\n");
     return (0);
 }
